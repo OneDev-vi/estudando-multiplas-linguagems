d26b18432fad6d3f069ed703eec7ef588ec45b63
Niko <shot.email>
2024-12-12 19:38:51 -0300

    so estudando denovo kkkk

---
+++ lua/coisas.lua
@@ -0,0 +1,14 @@
+print("Qual o seu nome??")
+local nome = tostring(io.read())
+print("Prazer em te conhecer "..nome)
+
+array = {}
+array[0] = "sal"
+array[1] = 10
+print(array[0])
+
+
+--[
+--sal
+--caju
+--]
